e64d754430df36b4f7f701f0f8b124b7ca039fe0
zimka1 <zimkail2015@mail.ru>
2024-10-19 00:07:45 +0200

    I structured data into packets for transmission over UDP, including metadata like type, ID, length, fragment number, and payload.

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import time
 from queue import Queue
+from dataclasses import dataclass
 
 # Defining states
 STATE_INITIAL = "INITIAL"
@@ -9,19 +10,39 @@ STATE_WAIT_SYN_ACK = "WAIT_SYN_ACK"
 STATE_CONNECTED = "CONNECTED"
 STATE_WAIT_FIN_ACK = "WAIT_FIN_ACK"
 
+# Packet's types
+TYPE_MESSAGE = 0x01
+TYPE_ACK = 0x02
+TYPE_SYN = 0x03
+TYPE_SYN_ACK = 0x04
+TYPE_FIN = 0x05
+TYPE_FIN_ACK = 0x06
+TYPE_HEARTBEAT = 0x07
+TYPE_HEARTBEAT_ACK = 0x08
+
 state = STATE_INITIAL  # Initial state
-isPartnerHere = 0
 heartbeat_interval = 5
 heartbeat_interval_to_ans = 2
 max_missed_heartbeats = 3
 heartbeat_missed = 0
 heartbeat_received = 0
 
+
 # Mutex for preventing simultaneous execution
 lock = threading.Lock()
 
 # Message queue
 message_queue = Queue()
+
+@dataclass
+class ParsedPacket:
+    packet_type: int        # Packet type
+    packet_id: int          # Unique packet identifier
+    total_length: int       # Total message length
+    fragment_number: int    # Current fragment number
+    total_fragments: int    # Total number of fragments
+    payload: str            # Payload (message or data)
+
 
 def show_help():
     print("""
@@ -37,68 +58,100 @@ def change_state(new_state):
     # print(f"Changing state from {state} to {new_state}")
     state = new_state
 
-def receive_handshake(addr):
-    global isPartnerHere
+
+def create_packet(packet_type, total_length, fragment_number, total_fragments, packet_id, payload):
+
+    packet = f"{packet_type}:{packet_id}:{total_length}:{fragment_number}:{total_fragments}:{payload}"
+    return packet.encode('utf-8')
+
+
+
+def send_packet(packet_type, message="", fragment_number=0, total_fragments=0, packet_id=0):
+    packet = create_packet(packet_type, len(message),fragment_number, total_fragments, packet_id, message)
+    sock.sendto(packet, (remote_ip, remote_port))
+
+
+def parse_packet(packet):
+    packet = packet.decode('utf-8')  # Декодируем пакет из байтов в строку
+    parts = packet.split(':')
+
+    # Преобразуем части пакета в нужные типы
+    packet_type = int(parts[0])
+    packet_id = int(parts[1])
+    total_length = int(parts[2])
+    fragment_number = int(parts[3])
+    total_fragments = int(parts[4])
+    payload = parts[5]
+
+    return ParsedPacket(
+        packet_type=packet_type,
+        packet_id=packet_id,
+        total_length=total_length,
+        fragment_number=fragment_number,
+        total_fragments=total_fragments,
+        payload=payload
+    )
+
+
+def receive_handshake():
     with lock:
         # print("Received SYN")
         if state == STATE_INITIAL:
-            sock.sendto(b"SYN-ACK", addr)
+            send_packet(TYPE_SYN_ACK)
+
             change_state(STATE_WAIT_SYN_ACK)  # Transition to the state waiting for SYN-ACK
+
             data, addr = sock.recvfrom(1024)
-            if data == b"ACK":
+            packet = parse_packet(data)
+
+            if packet.packet_type == TYPE_ACK:
                 print("Handshake complete.")
-                isPartnerHere = 1
                 change_state(STATE_CONNECTED)  # Transition to CONNECTED state
 
 def continue_handshake():
-    global isPartnerHere
     with lock:
         if state == STATE_WAIT_SYN_ACK:
             # print("Received SYN-ACK, sending ACK")
-            sock.sendto(b"ACK", (remote_ip, remote_port))
+            send_packet(TYPE_ACK)
             print("Handshake complete.")
-            isPartnerHere = 1
             change_state(STATE_CONNECTED)  # Transition to CONNECTED state
 
-def receive_termination(addr):
-    global isPartnerHere
+def receive_termination():
     with lock:
         if state == STATE_CONNECTED:
             # print("Received FIN")
-            sock.sendto(b"FIN-ACK", addr)  # 2. Node 2: ACK → Node 1
-            sock.sendto(b"FIN", addr)  # 3. Node 2: FIN → Node 1
+            send_packet(TYPE_FIN_ACK)  # 2. Node 2: FIN-ACK → Node 1
+            send_packet(TYPE_FIN)  # 3. Node 2: FIN → Node 1
             change_state(STATE_WAIT_FIN_ACK)  # Transition to the state waiting for FIN-ACK
             data, addr = sock.recvfrom(1024)
-            if data == b"ACK":
-                isPartnerHere = 0
+            packet = parse_packet(data)
+            if packet.packet_type == TYPE_ACK:
                 print("Connection terminated.")
                 change_state(STATE_INITIAL)  # Transition to INITIAL state
 
 def continue_termination():
-    global isPartnerHere
     with lock:
         if state == STATE_WAIT_FIN_ACK:
             # print("Received ACK, waiting for FIN")
             data, addr = sock.recvfrom(1024)
-            if data == b"FIN":
+            packet = parse_packet(data)
+            if packet.packet_type == TYPE_FIN:
                 # print("Received FIN, sending final ACK")
-                sock.sendto(b"ACK", addr)  # 4. Node 1: ACK → Node 2
+                send_packet(TYPE_ACK)  # 4. Node 1: ACK → Node 2
                 print("Connection terminated.")
-                isPartnerHere = 0
                 change_state(STATE_INITIAL)  # Transition to INITIAL state
 
 def wait_for_heartbeat():
-    global isPartnerHere
     while True:
-        if isPartnerHere:
+        if state == STATE_CONNECTED:
             heartbeat_monitor()
 
 def heartbeat_monitor():
-    global heartbeat_missed, isPartnerHere, heartbeat_received
-    while isPartnerHere:
+    global heartbeat_missed, heartbeat_received
+    while state == STATE_CONNECTED:
         time.sleep(heartbeat_interval)
 
-        sock.sendto(b"HEARTBEAT", (remote_ip, remote_port))
+        send_packet(TYPE_HEARTBEAT)
 
         time.sleep(heartbeat_interval_to_ans)
 
@@ -113,7 +166,6 @@ def heartbeat_monitor():
         if heartbeat_missed >= max_missed_heartbeats:
             print("Connection lost. No response from the partner.")
             heartbeat_missed = 0
-            isPartnerHere = 0
             change_state(STATE_INITIAL)
             break
 
@@ -123,39 +175,41 @@ def receive_message():
     while True:
         data, addr = sock.recvfrom(1024)
 
+        packet = parse_packet(data)
+
         # if someone wants to reconnect
-        if data == b"SYN":
-            receive_handshake(addr)
+        if packet.packet_type == TYPE_SYN:
+            receive_handshake()
             continue
 
-        if data == b"SYN-ACK":
+        if packet.packet_type == TYPE_SYN_ACK:
             continue_handshake()
             continue
 
         # if someone wants to disconnect
-        if data == b"FIN":
-            receive_termination(addr)
+        if packet.packet_type == TYPE_FIN:
+            receive_termination()
             continue
 
-        if data == b"FIN-ACK":
+        if packet.packet_type == TYPE_FIN_ACK:
             continue_termination()
             continue
 
-        if data == b"HEARTBEAT" and isPartnerHere == 1:
+        if packet.packet_type == TYPE_HEARTBEAT and state == STATE_CONNECTED:
             # print("Received HEARTBEAT")
-            sock.sendto(b"HEARTBEAT-ACK", addr)
+            send_packet(TYPE_HEARTBEAT_ACK)
             continue
-        elif data == b"HEARTBEAT":
+        elif packet.packet_type == TYPE_HEARTBEAT:
             continue
 
-        if data == b"HEARTBEAT-ACK" and isPartnerHere == 1:
+        if packet.packet_type == TYPE_HEARTBEAT_ACK and state == STATE_CONNECTED:
             # print("Received HEARTBEAT-ACK")
             heartbeat_received = 1
             continue
-        elif data == b"HEARTBEAT-ACK":
+        elif packet.packet_type == TYPE_HEARTBEAT_ACK:
             continue
 
-        print(f"You got message from {addr}: {data.decode()}")
+        print(f"You got message from {addr}: {packet.payload}")
 
 def handle_commands():
     global isPartnerHere
@@ -166,7 +220,7 @@ def handle_commands():
         if command == "connect" and state == STATE_INITIAL:
             # print("Send SYN")
             with lock:  # Using lock
-                sock.sendto(b"SYN", (remote_ip, remote_port))
+                send_packet(TYPE_SYN)
                 change_state(STATE_WAIT_SYN_ACK)  # Transition to the state waiting for SYN-ACK
 
         elif command.startswith("send message") and state == STATE_CONNECTED:
@@ -175,8 +229,8 @@ def handle_commands():
 
         elif command == "disconnect" and state == STATE_CONNECTED:
             with lock:
-                print("Send FIN")
-                sock.sendto(b"FIN", (remote_ip, remote_port))
+                # print("Send FIN")
+                send_packet(TYPE_FIN)
                 change_state(STATE_WAIT_FIN_ACK)  # Transition to the state waiting for FIN-ACK
 
         elif command == "help":
@@ -191,7 +245,7 @@ def process_message_queue():
             if state == STATE_CONNECTED:  # Sending messages only in the CONNECTED state
                 message = message_queue.get()
                 print(f"Sending message: {message}")
-                sock.sendto(message.encode(), (remote_ip, remote_port))
+                send_packet(TYPE_MESSAGE, message)
 
 
 # Network settings
