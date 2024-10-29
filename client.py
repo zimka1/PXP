71c08bb1627feb3f979629f1fd74e73466936383
zimka1 <zimkail2015@mail.ru>
2024-10-30 00:59:51 +0100

    Now I can send files and messages in fragments

--- client.py
+++ client.py
@@ -4,25 +4,25 @@ import time
 from queue import Queue
 from dataclasses import dataclass
 
+
 # Defining states
-STATE_DISCONNECTED = "DISCONNECTED"   # State: disconnected
-STATE_WAIT_SYN_ACK = "WAIT_SYN_ACK"   # State: waiting for SYN-ACK
-STATE_CONNECTED = "CONNECTED"         # State: connected
-STATE_WAIT_FIN_ACK = "WAIT_FIN_ACK"   # State: waiting for FIN-ACK
+STATE_DISCONNECTED = "DISCONNECTED"  # State: disconnected
+STATE_WAIT_SYN_ACK = "WAIT_SYN_ACK"  # State: waiting for SYN-ACK
+STATE_CONNECTED = "CONNECTED"  # State: connected
+STATE_WAIT_FIN_ACK = "WAIT_FIN_ACK"  # State: waiting for FIN-ACK
 STATE_WAIT_ACK = "WAIT_ACK"
-STATE_SEND_NEXT_PACKET = "SEND_NEXT_PACKET"
 
 # Packet's types
-TYPE_MESSAGE = 0x01       # Packet type: message
-TYPE_ACK = 0x02           # Packet type: ACK (acknowledgment)
-TYPE_SYN = 0x03           # Packet type: SYN (connection request)
-TYPE_SYN_ACK = 0x04       # Packet type: SYN-ACK (response to SYN)
-TYPE_FIN = 0x05           # Packet type: FIN (connection termination request)
-TYPE_FIN_ACK = 0x06       # Packet type: FIN-ACK (final acknowledgment for FIN)
-TYPE_HEARTBEAT = 0x07     # Packet type: HEARTBEAT (keep-alive signal)
-TYPE_HEARTBEAT_ACK = 0x08 # Packet type: HEARTBEAT-ACK (acknowledgment for HEARTBEAT)
-TYPE_NACK = 0x09          # Packet type: NACK (negative acknowledgment)
-TYPE_DATA = 0x0A          # Packet type: DATA (file or data transmission)
+TYPE_MESSAGE = 0x01  # Packet type: message
+TYPE_ACK = 0x02  # Packet type: ACK (acknowledgment)
+TYPE_SYN = 0x03  # Packet type: SYN (connection request)
+TYPE_SYN_ACK = 0x04  # Packet type: SYN-ACK (response to SYN)
+TYPE_FIN = 0x05  # Packet type: FIN (connection termination request)
+TYPE_FIN_ACK = 0x06  # Packet type: FIN-ACK (final acknowledgment for FIN)
+TYPE_HEARTBEAT = 0x07  # Packet type: HEARTBEAT (keep-alive signal)
+TYPE_HEARTBEAT_ACK = 0x08  # Packet type: HEARTBEAT-ACK (acknowledgment for HEARTBEAT)
+TYPE_NACK = 0x09  # Packet type: NACK (negative acknowledgment)
+TYPE_DATA = 0x0A  # Packet type: DATA (file or data transmission)
 TYPE_FIN_FRAG = 0x0B
 
 state = STATE_DISCONNECTED  # Initial state
@@ -38,25 +38,31 @@ lock = threading.Lock()
 # Message queue for sending messages
 message_queue = Queue()
 
+
 @dataclass
 class ParsedPacket:
-    packet_flag: int        # Packet type
-    packet_id: int          # Unique packet identifier
-    sequence_number: int    # Current fragment number or sequence number
-    checksum: int           # Checksum for error detection
-    total_length: int       # Total payload length
-    payload: str            # Payload (message or data)
+    packet_flag: int  # Packet type
+    packet_id: int  # Unique packet identifier
+    sequence_number: int  # Current fragment number or sequence number
+    checksum: int  # Checksum for error detection
+    total_length: int  # Total payload length
+    payload: str  # Payload (message or data)
 
 
 @dataclass
 class received_message:
+    type: int
     message: str
     sequence_number: int
     allIsCorrect: bool
 
+
 packet_ID = 0
+canISendFragments = True
 allReceived = True
-fragments = [received_message("", 0, True) for _ in range(32768)]
+fragments = [received_message(0, "", 0, True) for _ in range(32768)]
+total_message = ["" for _ in range(32768)]
+
 
 
 def change_state(new_state):
@@ -66,11 +72,11 @@ def change_state(new_state):
 
 # Creates the packet with specified parameters and encodes it to bytes
 def create_packet(packet_flag, packet_id, sequence_number, checksum, total_length, payload):
-    packet = packet_flag.to_bytes(1, 'big')        # Packet type (1 byte)
-    packet += packet_id.to_bytes(2, 'big')         # Packet ID (2 bytes)
-    packet += sequence_number.to_bytes(1, 'big')   # Sequence number (1 byte)
-    packet += checksum.to_bytes(2, 'big')          # Checksum (2 bytes)
-    packet += total_length.to_bytes(2, 'big')      # Total length (2 bytes)
+    packet = packet_flag.to_bytes(1, 'big')  # Packet type (1 byte)
+    packet += packet_id.to_bytes(2, 'big')  # Packet ID (2 bytes)
+    packet += sequence_number.to_bytes(1, 'big')  # Sequence number (1 byte)
+    packet += checksum.to_bytes(2, 'big')  # Checksum (2 bytes)
+    packet += total_length.to_bytes(2, 'big')  # Total length (2 bytes)
     packet += payload.encode('utf-8')
     return packet
 
@@ -85,11 +91,11 @@ def send_packet(packet_flag, payload="", packet_id=1, sequence_number=1, checksu
 # Parses the received packet and extracts the header and payload information
 def parse_packet(packet):
     packet_flag = int.from_bytes(packet[0:1], 'big')  # Packet type (1 byte)
-    packet_id = int.from_bytes(packet[1:3], 'big')    # Packet ID (2 bytes)
-    sequence_number = int.from_bytes(packet[3:4], 'big')   # Sequence number (1 byte)
-    checksum = int.from_bytes(packet[4:6], 'big')     # Checksum (2 bytes)
-    total_length = int.from_bytes(packet[6:8], 'big') # Total length (2 bytes)
-    payload = packet[8:].decode('utf-8')              # Payload (decoded as UTF-8)
+    packet_id = int.from_bytes(packet[1:3], 'big')  # Packet ID (2 bytes)
+    sequence_number = int.from_bytes(packet[3:4], 'big')  # Sequence number (1 byte)
+    checksum = int.from_bytes(packet[4:6], 'big')  # Checksum (2 bytes)
+    total_length = int.from_bytes(packet[6:8], 'big')  # Total length (2 bytes)
+    payload = packet[8:].decode('utf-8')  # Payload (decoded as UTF-8)
 
     return ParsedPacket(
         packet_flag=packet_flag,
@@ -106,7 +112,7 @@ def show_help():
     print("""
 Available commands:
 1. connect - Initiate connection to the partner.
-2. send message <text> - Send a message to the partner (only available when connected).
+2. send message (<message>, <length of fragments>) - Send a message to the partner (only available when connected).
 3. send file (<file name>, <length of fragments>) - Send a file to the partner (only available when connected).
 3. disconnect - Gracefully disconnect from the partner.
 4. help - Show this list of commands.
@@ -128,6 +134,7 @@ def receive_handshake():
                 print("Handshake complete.")
                 change_state(STATE_CONNECTED)  # Move to CONNECTED state
 
+
 # Sends SYN and waits for SYN-ACK to complete the handshake
 def continue_handshake():
     with lock:
@@ -142,7 +149,7 @@ def receive_termination():
     with lock:
         if state == STATE_CONNECTED:
             send_packet(TYPE_FIN_ACK)  # Send FIN-ACK
-            send_packet(TYPE_FIN)      # Send FIN
+            send_packet(TYPE_FIN)  # Send FIN
             change_state(STATE_WAIT_FIN_ACK)  # Move to WAIT_FIN_ACK state
 
             # Wait for final ACK
@@ -170,6 +177,7 @@ def wait_for_heartbeat():
     while True:
         if state == STATE_CONNECTED:
             heartbeat_monitor()
+
 
 # Handles sending and receiving heartbeat packets
 def heartbeat_monitor():
@@ -209,14 +217,15 @@ def calculate_crc16(data: bytes) -> int:
             crc &= 0xFFFF  # Make sure, that it's 16-bites value
     return crc
 
+
 def wait_for_ack():
     while True:
-        if state == STATE_CONNECTED:
+        if canISendFragments:
             break
 
 
 def send_file(file_name, fragment_length):
-    global packet_ID, state
+    global packet_ID, state, canISendFragments
 
     packet_ID += 1
 
@@ -234,7 +243,7 @@ def send_file(file_name, fragment_length):
                 sequence_number += 1
                 send_packet(TYPE_DATA, data.decode('utf-8'), packet_ID, sequence_number, crc)
                 if sequence_number == 6:
-                    change_state(STATE_WAIT_ACK)
+                    canISendFragments = False
                     wait_for_ack()
                     sequence_number = 0
             else:
@@ -243,13 +252,61 @@ def send_file(file_name, fragment_length):
                     sequence_number += 1
                     send_packet(TYPE_DATA, data.decode('utf-8'), packet_ID, sequence_number, crc)
                     if sequence_number == 6:
-                        change_state(STATE_WAIT_ACK)
+                        canISendFragments = False
                         wait_for_ack()
                         sequence_number = 0
+
+    if sequence_number != 0:
+        send_packet(TYPE_FIN_FRAG, "", packet_ID)
+        canISendFragments = False
+        wait_for_ack()
+    else:
+        send_packet(TYPE_FIN_FRAG, "", packet_ID)
 
     print("Partner has all fragments!")
 
 
+def send_message(message, fragment_length):
+    global packet_ID, state, canISendFragments
+
+    packet_ID += 1
+    last_fragments = []
+    sequence_number = 0
+    position = 0
+
+    while position < len(message):
+        if allReceived:
+            fragment = message[position:min(position + fragment_length, len(message))]
+            last_fragments.append(fragment)
+            crc = calculate_crc16(fragment.encode('utf-8'))
+            sequence_number += 1
+
+            send_packet(TYPE_MESSAGE, fragment, packet_ID, sequence_number, crc)
+
+            position += fragment_length
+            if sequence_number == 6:
+                canISendFragments = False
+                wait_for_ack()
+                sequence_number = 0
+        else:
+            for fragment in last_fragments:
+                crc = calculate_crc16(fragment.encode('utf-8'))
+                sequence_number += 1
+                send_packet(TYPE_MESSAGE, fragment, packet_ID, sequence_number, crc)
+
+                if sequence_number == 6:
+                    canISendFragments = False
+                    wait_for_ack()
+                    sequence_number = 0
+
+    if sequence_number != 0:
+        send_packet(TYPE_FIN_FRAG, "", packet_ID)
+        canISendFragments = False
+        wait_for_ack()
+    else:
+        send_packet(TYPE_FIN_FRAG, "", packet_ID)
+
+    print("Partner has received all fragments!")
 
 def checkIfSomethingIsWrong(packet):
     calculated_crc = calculate_crc16(packet.payload.encode('utf-8'))
@@ -257,26 +314,46 @@ def checkIfSomethingIsWrong(packet):
     if calculated_crc != packet.checksum:
         fragments[packet.packet_id].allIsCorrect = False
 
+    fragments[packet.packet_id].type = packet.packet_flag
     fragments[packet.packet_id].message += packet.payload
     fragments[packet.packet_id].sequence_number += 1
 
-    if fragments[packet.packet_id].sequence_number >= 6:
+    if fragments[packet.packet_id].sequence_number == 6:
         if fragments[packet.packet_id].allIsCorrect:
-            with open("recieved" + str(packet.packet_id), "a") as file:
-                file.write(fragments[packet.packet_id].message)
+
+            if fragments[packet.packet_id].type == TYPE_DATA:
+                with open("recieved" + str(packet.packet_id), "a") as file:
+                    file.write(fragments[packet.packet_id].message)
+            else:
+                total_message[packet.packet_id] += fragments[packet.packet_id].message
+
             send_packet(TYPE_ACK)
         else:
             send_packet(TYPE_NACK)
-
-        fragments[packet.packet_id] = received_message("", 0, True)
-
+        fragments[packet.packet_id] = received_message(fragments[packet.packet_id].type, "", 0, True)
 
 
+def save_data(packet):
+    if fragments[packet.packet_id].message != "" and fragments[packet.packet_id].allIsCorrect:
+        if fragments[packet.packet_id].type == TYPE_DATA:
+            with open("recieved" + str(packet.packet_id), "a") as file:
+                file.write(fragments[packet.packet_id].message)
+        else:
+            total_message[packet.packet_id] += fragments[packet.packet_id].message
+        send_packet(TYPE_ACK)
+    else:
+        send_packet(TYPE_NACK)
+
+    if fragments[packet.packet_id].type == TYPE_MESSAGE:
+        print(f"You got message: {total_message[packet.packet_id]}")
+        total_message[packet.packet_id] = ""
+
+    fragments[packet.packet_id] = received_message(0, "", 0, True)
 
 
 # Receives packets and handles the protocol logic for various packet types
 def receive_message():
-    global heartbeat_received, allReceived
+    global heartbeat_received, allReceived, canISendFragments
 
     while True:
         data, addr = sock.recvfrom(1024)  # Receive packet
@@ -284,12 +361,12 @@ def receive_message():
         packet = parse_packet(data)
 
         if packet.packet_flag == TYPE_ACK:
-            change_state(STATE_CONNECTED)
+            canISendFragments = True
             allReceived = True
             continue
 
         if packet.packet_flag == TYPE_NACK:
-            change_state(STATE_CONNECTED)
+            canISendFragments = True
             allReceived = False
             continue
 
@@ -326,11 +403,14 @@ def receive_message():
         elif packet.packet_flag == TYPE_HEARTBEAT_ACK:
             continue
 
-        if packet.packet_flag == TYPE_DATA:
+        if packet.packet_flag == TYPE_DATA or packet.packet_flag == TYPE_MESSAGE:
             checkIfSomethingIsWrong(packet)
             continue
 
-        print(f"You got message from {addr}: {packet.payload}")  # Print received message
+        if packet.packet_flag == TYPE_FIN_FRAG:
+            save_data(packet)
+
+
 
 def handle_commands():
     print("Type 'help' to see the list of available commands.")
@@ -338,14 +418,15 @@ def handle_commands():
         command = input().strip().lower()
 
         if command == "connect" and state == STATE_DISCONNECTED:
-            # print("Send SYN")
             with lock:  # Using lock
                 send_packet(TYPE_SYN)
                 change_state(STATE_WAIT_SYN_ACK)  # Transition to the state waiting for SYN-ACK
 
         elif command.startswith("send message") and state == STATE_CONNECTED:
-            message = command[len("send message "):]  # Extracting message text
-            message_queue.put(message)  # Putting the message into the queue
+            command = command.replace("send message (", "").replace(")", "")
+            message, fragment_length = command.split(", ")
+            send_message(message, int(fragment_length))
+            # message_queue.put(message)  # Putting the message into the queue
 
         elif command.startswith("send file") and state == STATE_CONNECTED:
             command = command.replace("send file (", "").replace(")", "")
@@ -366,13 +447,14 @@ def handle_commands():
         else:
             print("Invalid command or inappropriate state. Type 'help' for the list of available commands.")
 
-def process_message_queue():
-    while True:
-        if not message_queue.empty():
-            if state == STATE_CONNECTED:  # Sending messages only in the CONNECTED state
-                message = message_queue.get()
-                print(f"Sending message: {message}")
-                send_packet(TYPE_MESSAGE, message)
+
+# def process_message_queue():
+#     while True:
+#         if not message_queue.empty():
+#             if state == STATE_CONNECTED:  # Sending messages only in the CONNECTED state
+#                 message = message_queue.get()
+#                 print(f"Sending message: {message}")
+#                 send_packet(TYPE_MESSAGE, message)
 
 
 # Network settings
